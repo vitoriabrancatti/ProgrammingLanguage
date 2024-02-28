3ea6d0a5607f407e2080f8f12387f781600aae61
Batista <Vitoria.Batista@br.nsg.com>
2024-02-28 19:58:09 -0300

    Exercicio 1 - baralho

---
+++ Baralho/Main.java
@@ -0,0 +1,9 @@
+import org.example.enums.Naipe;
+
+@SuppressWarnings("unused")
+public class Main {
+    public static void main(String[] args) {
+        Baralho baralho = new Baralho();
+        System.out.println(baralho.toString());
+    }
+}
